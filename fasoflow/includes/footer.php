52e0bcbde5220e09784c3c52185facd094923006
Hasmed <hasmeds@gmail.com>
2026-03-02 13:32:55 +0000

    Modify footer with author details and formatting
    Updated footer to include author information and fixed formatting.

--- fasoflow/includes/footer.php
+++ fasoflow/includes/footer.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 
 <footer class="border-top py-3 bg-white">
   <div class="container small text-muted d-flex justify-content-between">
-    <div>© <?= date('Y') ?> FasoFlow</div>
+    <div>© <?= date('Y') ?> FasoFlow/Hasmed SAWADOGO, Expert en Digitalisation, gestion logistique et marchés publics</div>
     <div>Système d’automatisation des tâches</div>
   </div>
 </footer>
@@ -14,4 +14,5 @@ declare(strict_types=1);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/script.js"></script>
 </body>
+
 </html>
